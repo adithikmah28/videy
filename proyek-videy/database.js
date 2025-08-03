251bc17695eca54524901b318690f9a08fa7cfef
BioskopBoxOffice <hikmahtiaraditya@gmail.com>
2025-08-03 20:28:53 +0700

    Create database.js

---
+++ proyek-videy/database.js
@@ -0,0 +1,23 @@
+// File: database.js
+
+const videoList = {
+    // CONTOH REDIRECT LINK
+    "p3w9rntb2z": {
+        type: 'redirect',
+        title: 'Loading...',
+        url: 'https://www.profitableratecpm.com/n628mmmy13?key=cf801284c3d129d36815d85e16c550f8'
+    },
+    // CONTOH VIDEO BIASA
+    "2xtqstiqc1": {
+        type: 'video',
+        title: "Contoh Video Keren",
+        filename: "video-keren.mp4" 
+    },
+    "u7xje8fka9": {
+        type: 'video',
+        title: "Contoh Video Lainnya",
+        filename: "video-lainnya.mp4" 
+    }
+    // Tambahkan semua video dan link barumu di sini.
+    // File ini sekarang menjadi pusat datamu.
+};
